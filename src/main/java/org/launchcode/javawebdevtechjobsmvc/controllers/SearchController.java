2725c95560aa7393db414e8523770b5adf69c5e3
SkOoLyD <79675305+SkOoLyD@users.noreply.github.com>
2021-06-17 20:15:16 -0500

    completed #3 have to finish adding to search.html

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -23,6 +23,25 @@ public class SearchController {
         return "search";
     }
 
-    // TODO #3 - Create a handler to process a search request and render the updated search view.
+//     TODO #3 - Create a handler to process a search request and render the updated search view.
 
+    @PostMapping("/results")
+        public String displaySearchResults(Model model,@RequestParam String searchType,@RequestParam String searchTerm ) {
+        ArrayList<Job> jobs;
+        String radio = searchType;
+
+        if(searchType.toLowerCase().equals("all") && searchTerm.toLowerCase().equals("all") || searchTerm.toLowerCase().equals("")) {
+            jobs = JobData.findAll();
+            model.addAttribute("title"," searchType "+" searchTerm");
+        } else{
+            jobs = JobData.findByColumnAndValue(searchType,searchTerm);
+            model.addAttribute("title"," searchType "+" searchTerm");
+        }
+        model.addAttribute("radio",radio);
+        model.addAttribute("columns",columnChoices);
+        model.addAttribute("jobs", jobs);
+
+        return "search";
+
+    }
 }
